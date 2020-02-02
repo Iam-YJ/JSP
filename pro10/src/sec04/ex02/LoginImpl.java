062711349a795cb427fd31209d4a4b3449290451
Iam-YJ <yujin353@naver.com>
2020-02-02 20:05:50 +0900

    Firt commit
    Init

---
+++ pro10/src/sec04/ex02/LoginImpl.java
@@ -0,0 +1,49 @@
+package sec04.ex02;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class LoginImpl
+ *
+ */
+@WebListener
+public class LoginImpl implements HttpSessionListener {
+	String user_id;
+	String user_pw;
+	static int total_user;
+	
+    /**
+     * Default constructor. 
+     */
+    public LoginImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public LoginImpl(String user_id, String user_pw) {
+        // TODO Auto-generated constructor stub
+    	this.user_id = user_id;
+    	this.user_pw = user_pw;
+    	
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("技记 积己");
+    	++total_user;
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("技记 家戈");
+    	--total_user;
+    }
+	
+}
